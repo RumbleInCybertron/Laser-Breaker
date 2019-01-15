f9e1aae305c0e228369f6f1b16164f35ea09f986
Derek Haus <rumbleincybertron@gmail.com>
2019-01-16 00:03:58 +0900

    Create List of Waypoints

---
+++ Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    [SerializeField] GameObject path;
+    List<Transform> waypoints = new List<Transform>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int waypointIndex = 0;
+        foreach(Transform t in path.GetComponentInChildren<Transform>())
+        {
+            waypoints.Add(t);
+        }
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
