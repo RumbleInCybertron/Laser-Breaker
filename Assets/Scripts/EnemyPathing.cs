8e80f10d1e5e1778d3dd24047ef8dc88341380f9
Derek Haus <rumbleincybertron@gmail.com>
2019-01-16 12:08:17 +0900

    WaveConfig Scriptable Object

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class EnemyPathing : MonoBehaviour
 {
     [SerializeField] GameObject path;
+    [SerializeField] float moveSpeed = 2f;
     List<Transform> waypoints = new List<Transform>();
+
+    int waypointIndex = 0;
 
     // Start is called before the first frame update
     void Start()
-    {
-        int waypointIndex = 0;
+    { 
         foreach(Transform t in path.GetComponentInChildren<Transform>())
         {
             waypoints.Add(t);
@@ -21,6 +23,25 @@ public class EnemyPathing : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            var targetPosition = waypoints[waypointIndex].transform.position;
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
