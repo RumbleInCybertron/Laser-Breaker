e159d58f1bf571e619078d4bbcdc0f57c79b566d
Derek Haus <rumbleincybertron@gmail.com>
2019-01-24 17:38:43 +0900

    Display Player Health
    TODO: Health is sometimes displayed as a negative number.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -84,6 +84,11 @@ public class Player : MonoBehaviour
         }
     }
 
+    public int GetHealth()
+    {
+        return health;
+    }
+
     private void Die()
     {
         FindObjectOfType<Level>().LoadGameOver();
